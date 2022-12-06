bbd3379b7000a6d50aa872e45a04345bf32f7b59
Nitinjiwnani <jiwnani01@gmail.com>
2022-12-06 16:13:33 +0530

    Login Design UI

--- lib/UI/auth/login_screen.dart
+++ lib/UI/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_firebase/widgets/round_button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passswordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +30,57 @@ class _LoginScreenState extends State<LoginScreen> {
         automaticallyImplyLeading: false,
         title: Text('Login'),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        hintText: 'Email',
+                        helperText: 'enter email e.g john@gmail.com',
+                        prefixIcon: Icon(Icons.alternate_email)),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter email';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passswordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: 'password', prefixIcon: Icon(Icons.lock)),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter Password';
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            RoundButton(
+              title: 'Login',
+              onTap: () {
+                if (_formKey.currentState!.validate()) ;
+              },
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/round_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RoundButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  const RoundButton({super.key, required this.title, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Colors.deepPurple),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
