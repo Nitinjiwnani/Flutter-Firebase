82d04c5c3ab9ef3153929d98918d5b76f4d986cd
Nitinjiwnani <jiwnani01@gmail.com>
2022-12-06 17:21:01 +0530

    Login with firebase

--- lib/UI/auth/login_screen.dart
+++ lib/UI/auth/login_screen.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_firebase/UI/auth/singup_screen.dart';
+import 'package:flutter_firebase/UI/posts/post_screen.dart';
+import 'package:flutter_firebase/utils/utils.dart';
 import 'package:flutter_firebase/widgets/round_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,9 +16,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool loading = false;
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passswordController = TextEditingController();
+
+  final _auth = FirebaseAuth.instance;
 
   @override
   void dispose() {
@@ -23,6 +29,34 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
     emailController.dispose();
     passswordController.dispose();
+  }
+
+  void login() {
+    setState(() {
+      loading = true;
+    });
+    _auth
+        .signInWithEmailAndPassword(
+            email: emailController.text,
+            password: passswordController.text.toString())
+        .then((value) {
+      Utils().toastMessage(value.user!.email.toString());
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PostScreen(),
+          ));
+      setState(() {
+        loading = true;
+      });
+    }).onError((error, stackTrace) {
+      debugPrint(error.toString());
+      Utils().toastMessage(error.toString());
+      setState(() {
+        loading = true;
+      });
+    });
   }
 
   @override
@@ -82,8 +116,11 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               RoundButton(
                 title: 'Login',
+                loading: loading,
                 onTap: () {
-                  if (_formKey.currentState!.validate()) ;
+                  if (_formKey.currentState!.validate()) {
+                    login();
+                  }
                 },
               ),
               const SizedBox(
