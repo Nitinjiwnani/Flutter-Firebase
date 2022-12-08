80e83fd14c89ea46e5dd1dbcf0e59078073696c3
Nitinjiwnani <jiwnani01@gmail.com>
2022-12-08 19:28:44 +0530

    Realtime database CRUD operation

---
+++ lib/UI/posts/add_posts.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_firebase/utils/utils.dart';
+import 'package:flutter_firebase/widgets/round_button.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  final postController = TextEditingController();
+  bool loading = false;
+  final databaseRef = FirebaseDatabase.instance.ref('Post');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Post'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: postController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                  hintText: "What's on your mind?",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                title: 'Add',
+                loading: loading,
+                onTap: () {
+                  setState(() {
+                    loading = true;
+                  });
+                  databaseRef
+                      .child(DateTime.now().millisecondsSinceEpoch.toString())
+                      .child('comments')
+                      .set({
+                    'title': postController.text.toString(),
+                    'id': 1
+                  }).then((value) {
+                    setState(() {
+                      loading = false;
+                    });
+                    Utils().toastMessage('Post added');
+                  }).onError((error, stackTrace) {
+                    setState(() {
+                      loading = false;
+                    });
+                    Utils().toastMessage(error.toString());
+                  });
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
