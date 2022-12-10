27312d8a761276a202994c2574b90f0d03ebeabb
Nitinjiwnani <jiwnani01@gmail.com>
2022-12-10 17:01:23 +0530

    Add/insert or store data in firebase firestore

---
+++ lib/UI/firestore/firestore_list_screen.dart
@@ -0,0 +1,113 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_firebase/UI/auth/login_screen.dart';
+import 'package:flutter_firebase/UI/firestore/add_firestore_data.dart';
+import 'package:flutter_firebase/UI/posts/add_posts.dart';
+import 'package:flutter_firebase/utils/utils.dart';
+
+class FireStoreScreen extends StatefulWidget {
+  const FireStoreScreen({super.key});
+
+  @override
+  State<FireStoreScreen> createState() => _FireStoreScreenState();
+}
+
+class _FireStoreScreenState extends State<FireStoreScreen> {
+  final auth = FirebaseAuth.instance;
+  final editController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text('FireStore Screen'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                auth.signOut().then((value) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                      ));
+                }).onError((error, stackTrace) {
+                  Utils().toastMessage(error.toString());
+                });
+              },
+              icon: Icon(Icons.logout_outlined)),
+          SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddFireStoreDataScreen(),
+              ));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Nitin'),
+              );
+            },
+          )),
+        ],
+      ),
+    );
+  }
+
+  Future<void> showMyDialog(String title, String id) async {
+    editController.text = title;
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Update'),
+          content: Container(
+            child: TextField(
+              controller: editController,
+              decoration: InputDecoration(hintText: 'Edit'),
+            ),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Update')),
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/UI/auth/login_screen.dart';
+import 'package:flutter_firebase/UI/firestore/firestore_list_screen.dart';
 import 'package:flutter_firebase/UI/posts/post_screen.dart';
 
 class SplashServices {
@@ -18,7 +19,7 @@ class SplashServices {
           () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => PostScreen(),
+                builder: (context) => FireStoreScreen(),
               )));
     } else {
       Timer(
